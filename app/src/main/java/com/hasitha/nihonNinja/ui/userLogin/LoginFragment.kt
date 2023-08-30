56355862f857473fbe81dac9d7e5000047660cd0
Hasitha-Su <hasithakms@gmail.com>
2023-08-30 13:55:16 +0530

    Remove boilerplate code and re-structure the project

---
+++ app/src/main/java/com/hasitha/nihonNinja/ui/userLogin/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.hasitha.nihonNinja.ui.userLogin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.hasitha.nihonNinja.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button2).setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_dashboardFragment)
+        }
+    }
+}
