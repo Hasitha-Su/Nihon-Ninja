acc7a4a7f269d04f2ad5e06d8c49d31338ec64de
Hasitha-Su <hasithakms@gmail.com>
2023-09-05 09:07:25 +0530

    Implemented Dagger NetworkModule providing a singleton instance of the base URL in the com.hasitha.nihonNinja.di package

---
+++ app/src/main/java/com/hasitha/nihonNinja/di/NetworkModule.kt
@@ -0,0 +1,22 @@
+package com.hasitha.nihonNinja.di
+
+import com.hasitha.nihonNinja.constants.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideBaseUrl(): String {
+        return BASE_URL
+    }
+
+
+
+}
