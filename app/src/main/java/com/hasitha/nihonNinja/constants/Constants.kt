c6b003b09a330f65ea5d2d8cb7b4c1718db1b580
Hasitha-KMS <hasithakms@absi.digital>
2023-09-18 01:40:49 +0530

    Update BASE_URL for API endpoints

--- app/src/main/java/com/hasitha/nihonNinja/constants/Constants.kt
+++ app/src/main/java/com/hasitha/nihonNinja/constants/Constants.kt
@@ -1,7 +1,7 @@
 package com.hasitha.nihonNinja.constants
 
 object Constants {
-    const val BASE_URL = ""
+    const val BASE_URL = "https://raw.githubusercontent.com/Hasitha-Su/Nihon-Ninja-Mock-API/main/"
     const val END_POINT = ""
 }
 
