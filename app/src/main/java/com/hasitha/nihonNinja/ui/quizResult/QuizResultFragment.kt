69a2b3378eae504992dd0b549b72081faa3c2433
Hasitha-KMS <hasithakms@absi.digital>
2023-09-18 01:55:40 +0530

    Create QuizResultFragment for displaying quiz results

---
+++ app/src/main/java/com/hasitha/nihonNinja/ui/quizResult/QuizResultFragment.kt
@@ -0,0 +1,28 @@
+package com.hasitha.nihonNinja.ui.quizResult
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hasitha.nihonNinja.R
+
+class QuizResultFragment : Fragment() {
+
+
+    private lateinit var viewModel: QuizResultViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_quiz_result, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(QuizResultViewModel::class.java)
+
+    }
+}
---
+++ app/src/main/java/com/hasitha/nihonNinja/ui/quizResult/QuizResultViewModel.kt
@@ -0,0 +1,7 @@
+package com.hasitha.nihonNinja.ui.quizResult
+
+import androidx.lifecycle.ViewModel
+
+class QuizResultViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
