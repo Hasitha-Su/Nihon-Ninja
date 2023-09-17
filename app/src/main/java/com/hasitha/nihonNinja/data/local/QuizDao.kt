0422f4aa8fbf475a6646f544455483b14ecb825a
Hasitha-KMS <hasithakms@absi.digital>
2023-09-18 01:53:04 +0530

    Update QuizDao with database operations for quizzes, questions, and answers

---
+++ app/src/main/java/com/hasitha/nihonNinja/data/local/QuizDao.kt
@@ -0,0 +1,36 @@
+package com.hasitha.nihonNinja.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.hasitha.nihonNinja.model.entities.AnswerEntity
+import com.hasitha.nihonNinja.model.entities.QuestionEntity
+import com.hasitha.nihonNinja.model.entities.QuestionWithAnswers
+import com.hasitha.nihonNinja.model.entities.QuizEntity
+
+@Dao
+interface QuizDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertQuiz(quiz: QuizEntity)
+
+    @Query("SELECT * FROM quiz_table")
+    fun getAllQuizzes(): LiveData<List<QuizEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertQuestion(question: QuestionEntity)
+
+    @Query("SELECT * FROM question_table WHERE quizId = :quizId")
+    fun getQuestionsForQuiz(quizId: Int): LiveData<List<QuestionEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAnswer(answer: AnswerEntity)
+
+    @Query("SELECT * FROM answer_table WHERE questionId = :questionId")
+    fun getAnswersForQuestion(questionId: Int): LiveData<List<AnswerEntity>>
+
+    // Query to get Questions with their Answers for a particular Quiz
+    @Transaction
+    @Query("SELECT * FROM question_table WHERE quizId = :quizId")
+    fun getQuestionsWithAnswersForQuiz(quizId: Int): LiveData<List<QuestionWithAnswers>>
+
+}
