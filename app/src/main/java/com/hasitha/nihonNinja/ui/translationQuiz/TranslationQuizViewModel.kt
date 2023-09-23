cbb28c8df4d80de4ba9c7d3c31794b676023ec60
Hasitha-KMS <hasithakms@absi.digital>
2023-09-23 18:15:45 +0530

    Refactor TranslationQuizViewModel in the Android App
    1. Removed the `lateinit` modifier for the `sentences2` property and the commented-out `sentences` property, as they are now initialized directly when fetching quiz data.
    
    2. Introduced a `nextQuestion` function to handle the progression to the next question by updating the `currentSentenceIndex` LiveData.

--- app/src/main/java/com/hasitha/nihonNinja/ui/translationQuiz/TranslationQuizViewModel.kt
+++ app/src/main/java/com/hasitha/nihonNinja/ui/translationQuiz/TranslationQuizViewModel.kt
@@ -4,17 +4,43 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hasitha.nihonNinja.model.entities.QuestionWithAnswers
+import com.hasitha.nihonNinja.model.entities.QuizWithQuestionsAndAnswers
 import com.hasitha.nihonNinja.repository.QuestionRepository
 import com.hasitha.nihonNinja.repository.QuizRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-
 
 @HiltViewModel
 class TranslationQuizViewModel @Inject constructor(
     private val quizRepository: QuizRepository
 ) : ViewModel() {
 
+    //TODO - Remove lateinit
+    lateinit var sentences2: LiveData<QuizWithQuestionsAndAnswers>
+    //lateinit var sentences: LiveData<List<QuestionWithAnswers>>
+    val currentSentenceIndex: MutableLiveData<Int> = MutableLiveData(0)
+
+//    private val _currentWords = MutableLiveData<List<String>>()
+//    val currentWords: LiveData<List<String>> get() = _currentWords
+
+
+    fun fetchSentences(quizId: Int) {
+        //TODO - Remove hardcoded ID
+//        sentences = quizRepository.getQuestionsWithAnswersForQuiz(1)
+        sentences2 = quizRepository.getQuizWithQuestionsAndAnswers(1)
+    }
+
+    fun nextQuestion() {
+        currentSentenceIndex.value = (currentSentenceIndex.value ?: 0) + 1
+    }
+
+//    fun updateWords(newWords: List<String>) {
+//        _currentWords.value = newWords
+//    }
+
+
+
+    /*
     private val _sentences = MutableLiveData<List<QuestionWithAnswers>>()
     val sentences: LiveData<List<QuestionWithAnswers>> get() = _sentences
 
@@ -24,4 +50,6 @@ class TranslationQuizViewModel @Inject constructor(
             _sentences.value = questions
         }
     }
+
+     */
 }
