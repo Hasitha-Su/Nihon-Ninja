744ddf0193bb4a42b167afbeffbe4931ab91dff0
Hasitha-KMS <hasithakms@absi.digital>
2023-09-24 15:11:04 +0530

    Change the base url

--- app/src/main/java/com/hasitha/nihonNinja/constants/Constants.kt
+++ app/src/main/java/com/hasitha/nihonNinja/constants/Constants.kt
@@ -1,7 +1,8 @@
 package com.hasitha.nihonNinja.constants
 
 object Constants {
-    const val BASE_URL = "https://raw.githubusercontent.com/Hasitha-Su/Nihon-Ninja-Mock-API/main/"
+//    const val BASE_URL = "https://raw.githubusercontent.com/Hasitha-Su/Nihon-Ninja-Mock-API/main/"
+    const val BASE_URL = "https://nihon-ninja.onrender.com/"
     const val END_POINT = ""
 }
 
