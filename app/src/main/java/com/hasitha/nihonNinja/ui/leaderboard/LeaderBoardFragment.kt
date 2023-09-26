c824613971ceadcd3dd0acf2cf2a47acfae6dbfb
Hasitha-KMS <hasithakms@absi.digital>
2023-09-26 18:00:58 +0530

    Set up the RecyclerView adapter, and observed leaderBoardItems in the LeaderBoardViewModel to update the adapter accordingly

--- app/src/main/java/com/hasitha/nihonNinja/ui/leaderboard/LeaderBoardFragment.kt
+++ app/src/main/java/com/hasitha/nihonNinja/ui/leaderboard/LeaderBoardFragment.kt
@@ -6,27 +6,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.hasitha.nihonNinja.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.hasitha.nihonNinja.databinding.FragmentLeaderBoardBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LeaderBoardFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = LeaderBoardFragment()
-    }
+    private var _binding: FragmentLeaderBoardBinding? = null
+    private lateinit var adapter: LeaderBoardAdapter
 
+    private val binding get() = _binding!!
     private lateinit var viewModel: LeaderBoardViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_leader_board, container, false)
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentLeaderBoardBinding.inflate(inflater, container, false) // Initialize _binding first.
+        adapter = LeaderBoardAdapter()
+        binding.recyclerView.adapter = adapter // Now, binding can be accessed.
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
         viewModel = ViewModelProvider(this).get(LeaderBoardViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        viewModel.leaderBoardItems.observe(viewLifecycleOwner) {
+            adapter.setItems(it)
+        }
+
+        return binding.root
     }
 
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
