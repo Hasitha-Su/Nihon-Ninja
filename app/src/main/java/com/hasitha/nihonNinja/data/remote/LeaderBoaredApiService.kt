968fc705c24083ca7ac66b59bcb26157e36ba845
Hasitha-KMS <hasithakms@absi.digital>
2023-09-26 18:03:16 +0530

    Added a suspend function getLeaderBoard to the LeaderBoaredApiService interface for retrieving leader board data from the server

--- app/src/main/java/com/hasitha/nihonNinja/data/remote/LeaderBoaredApiService.kt
+++ app/src/main/java/com/hasitha/nihonNinja/data/remote/LeaderBoaredApiService.kt
@@ -1,4 +1,11 @@
 package com.hasitha.nihonNinja.data.remote
 
+import com.hasitha.nihonNinja.model.api.LeaderBoardUserResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface LeaderBoaredApiService {
+    @GET("leaderboard")
+    suspend fun getLeaderBoard(): List<LeaderBoardUserResponse>
+
 }
