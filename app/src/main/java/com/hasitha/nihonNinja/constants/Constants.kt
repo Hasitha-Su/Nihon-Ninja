6cbb7cd193258855b9eb361f8b8d68dd2d0a1e34
Hasitha-KMS <hasithakms@absi.digital>
2023-10-10 21:56:40 +0530

    Add URL of backend service to test API calls locally

--- app/src/main/java/com/hasitha/nihonNinja/constants/Constants.kt
+++ app/src/main/java/com/hasitha/nihonNinja/constants/Constants.kt
@@ -3,6 +3,7 @@ package com.hasitha.nihonNinja.constants
 object Constants {
 //    const val BASE_URL = "https://raw.githubusercontent.com/Hasitha-Su/Nihon-Ninja-Mock-API/main/"
     const val BASE_URL = "https://nihon-ninja.onrender.com/"
+//    const val BASE_URL = "http://10.0.2.2:8080/"
     const val END_POINT = ""
 }
 
