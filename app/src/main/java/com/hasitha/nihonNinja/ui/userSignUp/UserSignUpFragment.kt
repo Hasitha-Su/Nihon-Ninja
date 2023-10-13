5b3dd83134bc468f5abb3cc3d22abcbf34175596
Hasitha-KMS <hasithakms@absi.digital>
2023-10-13 22:02:34 +0530

    Update data binding for user sign up

--- app/src/main/java/com/hasitha/nihonNinja/ui/userSignUp/UserSignUpFragment.kt
+++ app/src/main/java/com/hasitha/nihonNinja/ui/userSignUp/UserSignUpFragment.kt
@@ -1,9 +1,7 @@
 package com.hasitha.nihonNinja.ui.userSignUp
 
-import android.opengl.ETC1.isValid
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,13 +11,13 @@ import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
 import com.hasitha.nihonNinja.R
 import com.hasitha.nihonNinja.databinding.FragmentUserSignUpBinding
+import com.hasitha.nihonNinja.model.api.SignUpRequest
 import com.hasitha.nihonNinja.util.Common.Companion.isValidEmail
 import com.hasitha.nihonNinja.util.Common.Companion.isValidPassword
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class UserSignUpFragment : Fragment() {
-
 
     private lateinit var viewModel: UserSignUpViewModel
     private var _binding: FragmentUserSignUpBinding? = null
@@ -30,108 +28,88 @@ class UserSignUpFragment : Fragment() {
 
         viewModel = ViewModelProvider(this).get(UserSignUpViewModel::class.java)
 
-        // Name validation
-        binding.name.editText?.doOnTextChanged { text, _, _, _ ->
-            if (text.isNullOrEmpty()) {
+        fun validateName(): Boolean {
+            val name = binding.name.editText?.text.toString()
+            if (name.isEmpty()) {
                 binding.name.error = "Name is required"
-            } else {
-                binding.name.error = null
+                return false
             }
+            binding.name.error = null
+            return true
         }
 
-        // Email validation
-        binding.email.editText?.doOnTextChanged { text, _, _, _ ->
-            if (text?.isEmpty() == true) {
+        fun validateEmail(): Boolean {
+            val email = binding.email.editText?.text.toString()
+            if (email.isEmpty()) {
                 binding.email.error = "Email is required."
-            } else if (!isValidEmail(text.toString())) {
+                return false
+            } else if (!isValidEmail(email)) {
                 binding.email.error = "Invalid email format."
-            } else {
-                binding.email.error = null // Clear the error if email is valid
+                return false
             }
+            binding.email.error = null
+            return true
         }
 
-        // Password validation
-        binding.password.editText?.doOnTextChanged { text, _, _, _ ->
-            val password = text.toString()
+        fun validatePassword(): Boolean {
+            val password = binding.password.editText?.text.toString()
             if (password.isEmpty()) {
                 binding.password.error = "Password is required."
+                return false
             } else if (!isValidPassword(password)) {
                 binding.password.error = "Password must have at least 8 characters, 1 special character, 1 number, 1 uppercase, and 1 lowercase letter."
-            } else {
-                binding.password.error = null // Clear the error if password is valid
+                return false
             }
+            binding.password.error = null
+            return true
         }
 
-        // Confirm Password validation
-        binding.confPassword.editText?.doOnTextChanged { text, _, _, _ ->
-            if (text.toString() != binding.password.editText?.text.toString()) {
+        fun validateConfPassword(): Boolean {
+            if (binding.confPassword.editText?.text.toString() != binding.password.editText?.text.toString()) {
                 binding.confPassword.error = "Passwords do not match"
-            } else {
-                binding.confPassword.error = null
+                return false
             }
+            binding.confPassword.error = null
+            return true
         }
 
-// Handle Signup click
-//        binding.loginButton.setOnClickListener {
-//            if (isValid()) {
-//                // Do your signup logic here
-//            } else {
-//                Toast.makeText(context, "Please fix the errors before signing up", Toast.LENGTH_SHORT).show()
-//            }
-//        }
+        // Overall validation:
+        fun isValid(): Boolean {
+            return validateName() && validateEmail() && validatePassword() && validateConfPassword()
+        }
 
+        // Text change listeners that call the validation functions:
+        binding.name.editText?.doOnTextChanged { _, _, _, _ -> validateName() }
+        binding.email.editText?.doOnTextChanged { _, _, _, _ -> validateEmail() }
+        binding.password.editText?.doOnTextChanged { _, _, _, _ -> validatePassword() }
+        binding.confPassword.editText?.doOnTextChanged { _, _, _, _ -> validateConfPassword() }
 
-//        fun isValid(): Boolean {
-//            return binding.name.error.isNullOrEmpty() &&
-//                    binding.email.error.isNullOrEmpty() &&
-//                    binding.password.error.isNullOrEmpty() &&
-//                    binding.confPassword.error.isNullOrEmpty()
-//        }
+        // Handle Signup click:
+        binding.loginButton.setOnClickListener {
+            if (isValid()) {
+                val name = binding.name.editText?.text.toString()
+                val email = binding.email.editText?.text.toString()
+                val password = binding.password.editText?.text.toString()
 
-//        binding.email.editText?.doOnTextChanged { text, _, _, _ ->
-//            if (text?.isEmpty() == true) {
-//                binding.email.error = "Email is required."
-//            } else if (!isValidEmail(text.toString())) {
-//                binding.email.error = "Invalid email format."
-//            } else {
-//                binding.email.error = null // Clear the error if email is valid
-//            }
-//        }
+                val signUpRequest = SignUpRequest(name, email, password)
+                viewModel.signUp(signUpRequest)
 
-//        binding.password.editText?.doOnTextChanged { text, _, _, _ ->
-//            val password = text.toString()
-//            if (password.isEmpty()) {
-//                binding.password.error = "Password is required."
-//            } else if (!isValidPassword(password)) {
-//                binding.password.error = "Password must have at least 8 characters, 1 special character, 1 number, 1 uppercase, and 1 lowercase letter."
-//            } else {
-//                binding.password.error = null // Clear the error if password is valid
-//            }
-//        }
+                viewModel.signUpResponse.observe(viewLifecycleOwner) { response ->
+                    if (response != null) {
+                        Toast.makeText(context, "Signup Successful!", Toast.LENGTH_SHORT).show()
+                        findNavController().navigate(R.id.action_userSignUpFragment_to_dashboardFragment)
+                    }
+                }
 
-//        binding.loginButton.setOnClickListener {
-//            val hasEmailError = binding.email.error != null
-//            val hasPasswordError = binding.password.error != null
-//
-//            if (!hasEmailError && !hasPasswordError) {
-//                val email = binding.email.editText?.text.toString().trim()
-//                val password = binding.password.editText?.text.toString().trim()
-////                viewModel.loginUser(email, password)
-//            }
-//        }
-
-//        viewModel.loginResponse.observe(viewLifecycleOwner) { response ->
-//            // Handle the login response
-//            if (response.error == null && response.user != null) {
-//                // Successful login
-//                Log.d("+++ Successful login", response.toString())
-////                findNavController().navigate(R.id.action_loginFragment_to_dashboardFragment)
-//            } else {
-//                // Error during login
-//                Log.d("+++ Error during login", response.toString())
-//                Toast.makeText(context, "Error, Please try again...!", Toast.LENGTH_SHORT).show()
-//            }
-//        }
+                viewModel.error.observe(viewLifecycleOwner) { errorMsg ->
+                    if (!errorMsg.isNullOrEmpty()) {
+                        Toast.makeText(context, "Signup Failed: $errorMsg", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(context, "Please fill all fields correctly", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onCreateView(
@@ -141,18 +119,4 @@ class UserSignUpFragment : Fragment() {
         _binding = FragmentUserSignUpBinding.inflate(inflater, container, false)
         return binding.root
     }
-
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        return inflater.inflate(R.layout.fragment_user_sign_up, container, false)
-//    }
-//
-////    override fun onActivityCreated(savedInstanceState: Bundle?) {
-////        super.onActivityCreated(savedInstanceState)
-////        viewModel = ViewModelProvider(this).get(UserSignUpViewModel::class.java)
-////        // TODO: Use the ViewModel
-////    }
-
 }
--- app/src/main/java/com/hasitha/nihonNinja/ui/userSignUp/UserSignUpViewModel.kt
+++ app/src/main/java/com/hasitha/nihonNinja/ui/userSignUp/UserSignUpViewModel.kt
@@ -1,7 +1,35 @@
 package com.hasitha.nihonNinja.ui.userSignUp
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hasitha.nihonNinja.model.api.SignUpRequest
+import com.hasitha.nihonNinja.model.api.SignUpResponse
+import com.hasitha.nihonNinja.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class UserSignUpViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class UserSignUpViewModel @Inject constructor(private val userRepository: UserRepository) : ViewModel() {
+
+    // LiveData to hold the signup response
+    private val _signUpResponse = MutableLiveData<SignUpResponse>()
+    val signUpResponse: LiveData<SignUpResponse> get() = _signUpResponse
+
+    // LiveData to hold error messages
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> get() = _error
+
+    fun signUp(request: SignUpRequest) {
+        viewModelScope.launch {
+            try {
+                val response = userRepository.signUp(request)
+                _signUpResponse.value = response
+            } catch (e: Exception) {
+                _error.value = "Error signing up: ${e.message}"
+            }
+        }
+    }
 }
