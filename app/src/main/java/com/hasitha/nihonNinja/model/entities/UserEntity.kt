eeece2227653e4bd986fd45be862438dbd051ccf
Hasitha-KMS <hasithakms@absi.digital>
2023-10-13 18:54:20 +0530

    Add user's name to User entity class

--- app/src/main/java/com/hasitha/nihonNinja/model/entities/UserEntity.kt
+++ app/src/main/java/com/hasitha/nihonNinja/model/entities/UserEntity.kt
@@ -6,8 +6,8 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "users")
 data class UserEntity(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
-    val username: String,
-//    val name: String,
+//    val username: String,
+    val name: String,
     val email: String,
     // other fields here…
 )
