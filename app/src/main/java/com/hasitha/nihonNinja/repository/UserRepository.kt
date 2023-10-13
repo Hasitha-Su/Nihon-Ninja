ebf66a77219f4f9becd0b44a62b304468cfe4014
Hasitha-KMS <hasithakms@absi.digital>
2023-10-13 22:02:57 +0530

    Add user sign up functionality

--- app/src/main/java/com/hasitha/nihonNinja/repository/UserRepository.kt
+++ app/src/main/java/com/hasitha/nihonNinja/repository/UserRepository.kt
@@ -5,6 +5,8 @@ import com.hasitha.nihonNinja.data.local.UserDao
 import com.hasitha.nihonNinja.data.remote.UserApiService
 import com.hasitha.nihonNinja.model.api.LoginRequest
 import com.hasitha.nihonNinja.model.api.LoginResponse
+import com.hasitha.nihonNinja.model.api.SignUpRequest
+import com.hasitha.nihonNinja.model.api.SignUpResponse
 import com.hasitha.nihonNinja.model.api.UserResponse
 import com.hasitha.nihonNinja.model.entities.UserEntity
 import com.hasitha.nihonNinja.util.SharedPrefManager
@@ -26,8 +28,8 @@ class UserRepository @Inject constructor(
         // Assuming loginResponse contains a User object
 //        userDao.saveUser(loginResponse.user)
         loginResponse.user?.let { user ->
-            val userEntity = convertToEntity(user)
-            userDao.saveUser(userEntity)
+//            val userEntity = convertToEntity(user)
+            userDao.saveUser(user)
 
             // Save user ID to SharedPreferences
             sharedPrefManager.saveUserId(user.id)
@@ -38,14 +40,45 @@ class UserRepository @Inject constructor(
 //        return userApiService.loginUser(loginRequest)
     }
 
-    private fun convertToEntity(userResponse: UserResponse): UserEntity {
-        return UserEntity(
-            id = userResponse.id, // Adjust this to the actual id field in UserResponse
-            name = userResponse.name,
-//            username = userResponse.username, // Adjust this to the actual username field in UserResponse
-            email = userResponse.email // Adjust this to the actual email field in UserResponse
-            // Map other fields as necessary
-        )
+    suspend fun signUp(request: SignUpRequest): SignUpResponse {
+        val signUpResponse = userApiService.userSignUp(request)
+        Log.d("+++ signUpRequest", request.toString())
+        println("+++ Signup Response: $signUpResponse")
+
+        // Assuming signUpResponse contains a User object in the data field
+        signUpResponse.data.let { user ->
+//            val userEntity = convertUserToEntity(user)
+            userDao.saveUser(user)
+
+            // Save user ID to SharedPreferences
+            sharedPrefManager.saveUserId(user.id)
+            sharedPrefManager.saveUserName(user.name)
+        }
+
+        return signUpResponse
     }
+
+    // Assuming UserResponse and UserEntity are your data classes or types.
+//    private fun convertUserToEntity(user: UserResponse): UserEntity {
+//        return UserEntity(
+//            user.id,
+//            user.name,
+//            user.email
+//        )
+//    }
+
+
+
+
+
+//    private fun convertToEntity(userResponse: UserResponse): UserEntity {
+//        return UserEntity(
+//            id = userResponse.id, // Adjust this to the actual id field in UserResponse
+//            name = userResponse.name,
+////            username = userResponse.username, // Adjust this to the actual username field in UserResponse
+//            email = userResponse.email // Adjust this to the actual email field in UserResponse
+//            // Map other fields as necessary
+//        )
+//    }
 
 }
